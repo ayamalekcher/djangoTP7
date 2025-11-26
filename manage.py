1bea7ca3604771ecfece27ac4aae3728c6071107
ayamalekcher <ayamalek.cherchar@univ-constantine2.dz>
2025-11-26 12:58:08 +0100

    Deployment to Render

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
+
 #!/usr/bin/env python3
 import os, sys
-if __name__ == "__main__":
+if name == "main":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE","demoDjango.settings")
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
